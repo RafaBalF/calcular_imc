277b707e6c8d0369222fb129300bb9bfe08b805c
Rafael Baleeiro <100231558+RafaBalF@users.noreply.github.com>
2023-03-10 16:13:52 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,137 @@
-// ignore_for_file: prefer_const_constructors
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatefulWidget {
+class App extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<App> createState() => _AppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  TextEditingController txtAlturaCtrl = TextEditingController();
+class _AppState extends State<App> {
+  final txtPeso = TextEditingController();
+  final txtAltura = TextEditingController();
 
-  var txtPesoCtrl = TextEditingController();
+  double? imc;
+  String? mensagem;
 
-  String imageImc = "images/normal.jpg";
-  double imc = 0;
+  var imagem = 'images/ideal.jpg';
+
   void calculaImc() {
-    var altura = double.parse(txtAlturaCtrl.text);
-    var peso = double.parse(txtPesoCtrl.text);
-    // print(altura);
-    // print(peso);
+    var peso = double.parse(txtPeso.text);
+    var altura = double.parse(txtAltura.text);
 
     imc = peso / (altura * altura);
 
-    if (imc < 18.5)
-      imageImc = "images/magreza.jpg";
-    else if (imc < 25)
-      imageImc = "images/normal.jpg";
-    else if (imc < 30)
-      imageImc = "images/excesso.jpg";
-    else if (imc < 35)
-      imageImc = "images/obesidade.jpg";
-    else
-      imageImc = "images/obesidade2.jpg";
+    if (imc! <= 18.4) {
+      setState(() => imagem = 'images/abaixo.jpg');
+      setState(() => mensagem = 'Abaixo');
+    } else if (imc! >= 18.5 && imc! <= 24.9) {
+      setState(() => imagem = 'images/ideal.jpg');
+      setState(() => mensagem = 'Ideal');
+    } else if (imc! >= 25 && imc! < 29.9) {
+      setState(() => imagem = 'images/excesso.jpg');
+      setState(() => mensagem = 'Excesso');
+    } else if (imc! >= 30 && imc! <= 39.9) {
+      setState(() => imagem = 'images/obesidade1.jpg');
+      setState(() => mensagem = 'Obesidade 1');
+    } else if (imc! >= 40) {
+      setState(() => imagem = 'images/obesidade2.jpg');
+      setState(() => mensagem = 'Obesidade 2');
+    }
 
-    setState(() {});
+    setState(() => imc!.toStringAsFixed(2));
 
-    FocusManager.instance.primaryFocus?.unfocus();
+    print(imc);
+    print(imagem);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.yellow, scaffoldBackgroundColor: Colors.white),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: Text('IMC'),
+          title: const Text('Cálculo IMC'),
         ),
         body: Column(
           children: [
             Flexible(
-              child: Container(
-                margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                //color: Colors.blue,
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: txtAlturaCtrl,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Altura',
-                        labelText: 'Altura',
-                      ),
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  //color: Colors.black12,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: txtPeso,
+                          decoration: InputDecoration(
+                              labelText: 'Peso', hintText: 'Em kilos'),
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: txtAltura,
+                          decoration: InputDecoration(
+                              labelText: 'Altura', hintText: 'Em metros'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () => calculaImc(),
+                          child: Text('Calcular'),
+                        ),
+                      ],
                     ),
-                    TextField(
-                      controller: txtPesoCtrl,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Peso',
-                        labelText: 'Peso',
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        child: Text("Calcular IMC"),
-                        //onPressed: () => calculaImc(), --se tiver apenas um metodos trocar {} por => se retorno e parametros forem iguais faca que nem na linha dee baixo
-                        onPressed: calculaImc,
-                      ),
-                    ),
-                    Text(imc.toStringAsFixed(2)),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              //flex: 3,
-              child: Container(
-                child: Center(
-                  child: Image.asset(
-                    imageImc,
-                    scale: 0.5,
                   ),
                 ),
               ),
+            ),
+            Expanded(
+              flex: 2,
+              child: imc == null
+                  ? Container()
+                  : FittedBox(
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Image.asset(imagem),
+                          ),
+                          Center(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(top: 60.0),
+                                child: Text(
+                                  '$mensagem peso',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'IMC: ${imc!.toStringAsFixed(2)}',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          )),
+                        ],
+                      ),
+                    ),
             ),
           ],
         ),
